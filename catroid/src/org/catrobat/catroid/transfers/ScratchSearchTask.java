c6359fe741b7fcc5f9308795ebbd10c293b400dc
Ralph Samer <ralph.samer@student.tugraz.at>
2016-04-05 18:57:56 +0200

    CAT-1159 Refactored Scratch search async task

--- catroid/src/org/catrobat/catroid/transfers/ScratchSearchTask.java
+++ catroid/src/org/catrobat/catroid/transfers/ScratchSearchTask.java
@@ -23,67 +23,50 @@
 
 package org.catrobat.catroid.transfers;
 
-import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.util.Log;
-import android.view.Gravity;
-
 import org.catrobat.catroid.web.ServerCalls;
 
-import java.util.ArrayList;
+public class ScratchSearchTask extends AsyncTask<String, Integer, ServerCalls.ScratchSearchResult> {
+    public interface ScratchSearchTaskDelegate {
+        void onPreExecute();
+        void onPostExecute(ServerCalls.ScratchSearchResult result);
+    }
 
-public class ScratchSearchTask extends AsyncTask<String, Void, String> {
+    private ScratchSearchTaskDelegate delegate = null;
 
-    String textSearch;
-    ProgressDialog pd;
-    ArrayList<ServerCalls.ScratchProject> projectList;
+    public ScratchSearchTask setDelegate(ScratchSearchTaskDelegate delegate) {
+        this.delegate = delegate;
+        return this;
+    }
 
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-        projectList = new ArrayList<>();
-        /*
-        pd = new ProgressDialog(getActivity());
-        pd.setCancelable(false);
-        pd.setMessage("Searching...");
-        pd.getWindow().setGravity(Gravity.CENTER);
-        pd.show();
-        */
-    }
-
-    @Override
-    protected String doInBackground(String... sText) {
-        textSearch = sText[0];
-        String returnResult = getProjectList(textSearch);
-        return returnResult;
-    }
-
-    public String getProjectList(String query) {
-        try {
-            projectList.clear();
-            ServerCalls.ScratchSearchSortType sortType = ServerCalls.ScratchSearchSortType.RELEVANCE;
-            ServerCalls.ScratchSearchResult searchResult;
-            searchResult = ServerCalls.getInstance().scratchSearch(query, sortType, 20, 0);
-            projectList = searchResult.getProjectList();
-            return ("OK");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ("Exception Caught");
+        if (delegate != null) {
+            delegate.onPreExecute();
         }
     }
 
     @Override
-    protected void onPostExecute(String result) {
+    protected ServerCalls.ScratchSearchResult doInBackground(String... params) {
+        return fetchProjectList(params[0]);
+    }
+
+    public ServerCalls.ScratchSearchResult fetchProjectList(String query) {
+        try {
+            ServerCalls.ScratchSearchSortType sortType = ServerCalls.ScratchSearchSortType.RELEVANCE;
+            return ServerCalls.getInstance().scratchSearch(query, sortType, 20, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    protected void onPostExecute(ServerCalls.ScratchSearchResult result) {
         super.onPostExecute(result);
-        if (result.equalsIgnoreCase("Exception Caught")) {
-            //Toast.makeText(getActivity(), "Unable to connect to server,please try later", Toast.LENGTH_LONG).show();
-            //pd.dismiss();
-        } else {
-            for (ServerCalls.ScratchProject project : projectList) {
-                Log.i("S2CC", project.getTitle());
-            }
-            //searchResults.setAdapter(new SearchResultsAdapter(getActivity(),filteredProductResults));
-            //pd.dismiss();
+        if (delegate != null) {
+            delegate.onPostExecute(result);
         }
     }
 
