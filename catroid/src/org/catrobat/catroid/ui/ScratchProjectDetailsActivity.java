10d321fc6c873c4fb6b8e61844f9f6395c983265
Ralph Samer <ralph.samer@student.tugraz.at>
2016-05-13 22:34:10 +0200

    CAT-1923 Hook up new views + display new scratch project attribute data

--- catroid/src/org/catrobat/catroid/ui/ScratchProjectDetailsActivity.java
+++ catroid/src/org/catrobat/catroid/ui/ScratchProjectDetailsActivity.java
@@ -23,15 +23,13 @@
 
 package org.catrobat.catroid.ui;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
-import android.graphics.Bitmap;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -41,27 +39,35 @@ import org.catrobat.catroid.R;
 import org.catrobat.catroid.common.Constants;
 import org.catrobat.catroid.common.ScratchProjectData;
 import org.catrobat.catroid.common.ScratchProjectPreviewData;
-import org.catrobat.catroid.common.ScratchSearchResult;
 import org.catrobat.catroid.transfers.FetchScratchProjectDetailsTask;
-import org.catrobat.catroid.transfers.FetchScratchProjectsTask;
 import org.catrobat.catroid.utils.ExpiringDiskCache;
 import org.catrobat.catroid.utils.ExpiringLruMemoryImageCache;
-import org.catrobat.catroid.utils.ExpiringLruMemoryObjectCache;
 import org.catrobat.catroid.utils.ToastUtil;
 import org.catrobat.catroid.utils.WebImageLoader;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static android.view.View.*;
+
 public class ScratchProjectDetailsActivity extends BaseActivity
         implements FetchScratchProjectDetailsTask.ScratchProjectListTaskDelegate {
 
     private static final String TAG = ScratchProjectDetailsActivity.class.getSimpleName();
 
-    private TextView projectTitleTextView;
-    private ImageView projectImageView;
-    private TextView projectInstructionsTextView;
-    private TextView projectNotesAndCreditsTextView;
+    private TextView titleTextView;
+    private TextView ownerTextView;
+    private ImageView imageView;
+    private TextView instructionsTextView;
+    private TextView notesAndCreditsLabelView;
+    private TextView notesAndCreditsTextView;
+    private TextView favoritesTextView;
+    private TextView lovesTextView;
+    private TextView viewsTextView;
+    private TextView tagsTextView;
+    private TextView sharedTextView;
+    private TextView modifiedTextView;
+    private Button convertButton;
     private WebImageLoader webImageLoader;
     private ProgressDialog progressDialog;
     private FetchScratchProjectDetailsTask currentTask = null;
@@ -81,28 +87,42 @@ public class ScratchProjectDetailsActivity extends BaseActivity
         );
 
         Log.i(TAG, scratchProjectData.getTitle());
-        projectTitleTextView = (TextView) findViewById(R.id.scratch_project_title);
-        projectImageView = (ImageView) findViewById(R.id.scratch_project_image_view);
-        projectInstructionsTextView = (TextView) findViewById(R.id.scratch_project_instructions_text);
-        projectNotesAndCreditsTextView = (TextView) findViewById(R.id.scratch_project_notes_and_credits_text);
+        titleTextView = (TextView) findViewById(R.id.scratch_project_title);
+        ownerTextView = (TextView) findViewById(R.id.scratch_project_owner);
+        imageView = (ImageView) findViewById(R.id.scratch_project_image_view);
+        instructionsTextView = (TextView) findViewById(R.id.scratch_project_instructions_text);
+        notesAndCreditsLabelView = (TextView) findViewById(R.id.scratch_project_notes_and_credits_label);
+        notesAndCreditsTextView = (TextView) findViewById(R.id.scratch_project_notes_and_credits_text);
+        favoritesTextView = (TextView) findViewById(R.id.scratch_project_favorites_text);
+        lovesTextView = (TextView) findViewById(R.id.scratch_project_loves_text);
+        viewsTextView = (TextView) findViewById(R.id.scratch_project_views_text);
+        tagsTextView = (TextView) findViewById(R.id.scratch_project_tags_text);
+        sharedTextView = (TextView) findViewById(R.id.scratch_project_shared_text);
+        modifiedTextView = (TextView) findViewById(R.id.scratch_project_modified_text);
+        convertButton = (Button) findViewById(R.id.scratch_project_convert_button);
+
+        instructionsTextView.setText("-");
+        notesAndCreditsLabelView.setVisibility(INVISIBLE);
+        notesAndCreditsTextView.setVisibility(INVISIBLE);
+        tagsTextView.setVisibility(INVISIBLE);
 
         int width = getResources().getDimensionPixelSize(R.dimen.scratch_project_image_width);
         int height = getResources().getDimensionPixelSize(R.dimen.scratch_project_image_height);
 
+        // TODO: use LRU cache!
+        // TODO: show remixes! button and show in separate activity???
+
         if (scratchProjectData != null) {
-            projectTitleTextView.setText(scratchProjectData.getTitle());
+            titleTextView.setText(scratchProjectData.getTitle());
 
             if (scratchProjectData.getProjectImage() != null) {
                 webImageLoader.fetchAndShowImage(
                         scratchProjectData.getProjectImage().getUrl().toString(),
-                        projectImageView, width, height
+                        imageView, width, height
                 );
             }
         }
 
-        // TODO: consider pagination for cache!
-        // cache miss
-        Log.d(TAG, "Cache miss!");
         if (currentTask != null) {
             currentTask.cancel(true);
         }
@@ -153,19 +173,52 @@ public class ScratchProjectDetailsActivity extends BaseActivity
                 Preconditions.checkNotNull(progressDialog, "No progress dialog set/initialized!");
                 progressDialog.dismiss();
                 if (projectData == null) {
-                    ToastUtil.showError(activity, "Unable to connect to server, please try later");
+                    ToastUtil.showError(activity, activity.getString(R.string.error_scratch_project_data_not_available));
                     return;
                 }
 
-                activity.projectTitleTextView.setText(projectData.getTitle());
-                activity.projectInstructionsTextView.setText(projectData.getDescription());
-                activity.projectNotesAndCreditsTextView.setText(
-                        "Views: " + projectData.getViews() + "\n" +
-                                "Favorites: " + projectData.getFavorites() + "\n" +
-                                "Loves: " + projectData.getLoves() + "\n"
-                );
+                activity.titleTextView.setText(projectData.getTitle());
+                activity.ownerTextView.setText(activity.getString(R.string.by) + " " + projectData.getOwner());
+                final String instructionsText = projectData.getInstructions().replace("\n\n", "\n");
+                final String notesAndCredits = projectData.getNotesAndCredits().replace("\n\n", "\n");
+                activity.instructionsTextView.setText((instructionsText.length() > 0) ? instructionsText : "-");
+                if (notesAndCredits.length() > 0) {
+                    activity.notesAndCreditsTextView.setText(notesAndCredits);
+                    activity.notesAndCreditsLabelView.setVisibility(VISIBLE);
+                    activity.notesAndCreditsTextView.setVisibility(VISIBLE);
+                }
+                activity.favoritesTextView.setText(shortNumber(projectData.getFavorites()));
+                activity.lovesTextView.setText(shortNumber(projectData.getLoves()));
+                activity.viewsTextView.setText(shortNumber(projectData.getViews()));
+
+                StringBuilder tagList = new StringBuilder();
+                int index = 0;
+                for (String tag : projectData.getTags()) {
+                    tagList.append((index++ > 0 ? ", " : "") + tag);
+                }
+                if (tagList.length() > 0) {
+                    activity.tagsTextView.setText(tagList);
+                    activity.tagsTextView.setVisibility(VISIBLE);
+                }
+                Log.d(TAG, projectData.getModifiedDate());
+                Log.d(TAG, projectData.getSharedDate());
+                activity.sharedTextView.setText(activity.getString(R.string.shared) + ": " + projectData.getSharedDate());
+                activity.modifiedTextView.setText(activity.getString(R.string.modified) + ": " + projectData.getModifiedDate());
             }
         });
+    }
+
+    // TODO: improve and move this helper method to Util class
+    private static String shortNumber(final int number) {
+        if (number < 1_000) {
+            return Integer.toString(number);
+        } else if (number < 10_000) {
+            return Integer.toString(number/1_000) +
+                    (number%1000 > 100 ? "." + Integer.toString((number%1000)/100) : "") + "k";
+        } else if (number < 1_000_000) {
+            return Integer.toString(number/1_000) + "k";
+        }
+        return Integer.toString(number/1_000_000) + "M";
     }
 
 }
